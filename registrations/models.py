d2f146526bcee95169c5b400629f982a6fc15f5b
ignoredmanas <manas.dhaware28@gmail.com>
2026-09-15 10:36:39 +0530

    Add registration database model

--- registrations/models.py
+++ registrations/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Registrations(models.Model):
+    GAME_CHOICES = [
+        ("valorant","Valorant"),
+        ("bgmi","BGMI"),
+        ("free-fire","Free Fire"),
+    ]
+    team_name = models.CharField(max_length=1000)
+    game = models.CharField(max_length=20,choices=GAME_CHOICES)
+    leader_name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.team_name} - {self.game}"
+    
